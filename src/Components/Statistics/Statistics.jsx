951555742831ce18371924a9e5d6245497637ede
Muinul-Islam <muinul1022@gmail.com>
2023-09-25 22:38:19 +0600

    donation campaign react assignment

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,5 +1,24 @@
-const Statistics = () => {
-  return <div></div>;
+/* eslint-disable react/prop-types */
+import { PieChart } from "recharts";
+
+const Statistics = ({ Categories }) => {
+  console.log(Categories);
+  return (
+    <div>
+      <PieChart
+        series={[
+          {
+            data: [
+              { id: 0, value: 10, label: "series A" },
+              { id: 1, value: 15, label: "series B" },
+            ],
+          },
+        ]}
+        width={400}
+        height={200}
+      />
+    </div>
+  );
 };
 
 export default Statistics;
